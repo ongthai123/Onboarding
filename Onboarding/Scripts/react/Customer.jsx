02347500c4f364e557c504b43653e09e401ec1b1
Thai <ongthai2803@gmail.com>
2019-01-30 12:00:35 +1300

    first commit

---
+++ Onboarding/Scripts/react/Customer.jsx
@@ -0,0 +1,80 @@
+﻿import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class ListingTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openModal: false,
+            customerList: []
+        };
+        this.loadData = this.loadData.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData() {
+        //ajax call logic
+    }
+    update(id) {
+        //ajax call logic
+    }
+
+    delete(id) {
+        //ajax call logic
+    }
+
+    render() {
+
+        let customerList = this.state.customerList;
+
+        let tableData = null;
+
+        if (customerList != "") {
+            tableData = customerList.map(customer =>
+                <tr key={customer.id}>
+                    <td className="four wide">{service.name}</td>
+                    <td className="four wide">{service.adress}</td>
+                    <td className="four wide">
+                        <i className="outline write icon" onClick={this.update.bind(this, service.id)}></i>
+                    </td>
+                    <td>
+                        <i className="remove icon" onClick={this.delete.bind(this, service.id)}></i>
+                    </td>
+                </tr>
+            )
+        }
+        return (
+            <React.Fragment>
+                <div style={{ margin: '10px 0' }}>
+                    <button className="huge ui primary button">
+                        New Customer
+                    </button>
+                </div>
+                <div>
+                    <table className="ui striped table">
+                        <thead>
+                            <tr>
+                                <th className="four wide">Name</th>
+                                <th className="four wide">Address</th>
+                                <th className="four wide">Actions</th>
+                                <th className="four wide">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {tableData}
+                        </tbody>
+                    </table>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+var customerView = document.getElementById("customerView");
+ReactDOM.render(<ListingTable />, customerView);
---
+++ Onboarding/Scripts/react/webpack.config.js
@@ -0,0 +1,56 @@
+﻿module.exports = {
+    context: __dirname,
+    entry: {
+        index: __dirname + '/Customer.jsx'
+    },
+    output:
+    {
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js"
+    },
+    watch: true,
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            },
+            //{
+            //    test: /\.css$/,
+            //    loader: 'url-loader?limit=100000',
+            //},
+            //{
+            //    test: /\.css$/,
+            //    loader: 'style-loader!css-loader'
+            //},
+            //{
+            //    test: /\.s[a|c]ss$/,
+            //    loader: 'sass-loader!style-loader!css-loader'
+            //},
+            //{
+            //    test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+            //    loader: 'url-loader?limit=100000'
+            //}
+        ]
+    }
+
+    //plugins: [
+    //    new webpack.DefinePlugin({
+    //        //IDENTITY_API: JSON.stringify('http://52.187.246.133:60968'),
+    //        //PROFILE_API: JSON.stringify('http://52.187.246.133:60190'),
+    //        //LISTING_API: JSON.stringify('http://52.187.246.133:51689'),
+    //        //SCHEDULE_API: JSON.stringify('http://52.187.246.133:58297')
+    //        IDENTITY_API: JSON.stringify('http://localhost:60968'),
+    //        PROFILE_API: JSON.stringify('http://localhost:60190'),
+    //        LISTING_API: JSON.stringify('http://localhost:51689'),
+    //        SCHEDULE_API: JSON.stringify('http://localhost:58297')
+    //    })
+    //]
+}
